d6c0a5786685eab37eaa7c9d80eabc47d83fcc7b
ARN1380 <alireza.naghavi0@gmail.com>
2024-02-07 09:16:43 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-950 flex items-center justify-center">
+      <div className="text-[#ececec] text-3xl uppercase">we are going to have a wedding Inshallah :)</div>
+    </div>
+  );
+};
+
+export default App;
