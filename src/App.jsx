ed6e9074efd38f1155039d00a81b8f78671b3492
ali <alireza.naghavi0@gmail.com>
2024-09-07 08:10:08 +0330

    did some changes, actually made it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import image from "./assets/react.svg";
+import weddPic from "./assets/wedd.png";
+import BackgroundVid from "./assets/unblur-trim.mp4";
 
 const App = () => {
+  // const [anything] = useSound(mySound)
+
   return (
-    <div className="w-screen h-screen bg-blue-950 flex items-center justify-center">
-      <div className="text-[#ececec] text-3xl uppercase">we are going to have a wedding Inshallah :)</div>
+    <div className="w-screen h-screen bg-blue-950 flex items-center justify-center flex-col">
+      <div className="min-h-screen relative overflow-hidden">
+        <video className="absolute left-0 top-0 w-full h-full object-cover" autoPlay >
+          <source src={BackgroundVid} type="video/mp4" />
+        </video>
+        <div className="backdrop-blur-md backdrop-brightness-50 w-screen h-screen text-white flex items-center justify-center">
+          <img className="h-[75vh] min-w-fit animate-fade" src={weddPic} alt="not found" />
+        </div>
+      </div>
+
+      {/* <div className="max-w-96 backdrop-blur-sm z-10">
+      </div> */}
     </div>
   );
 };
