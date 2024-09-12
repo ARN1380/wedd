7d056360f4230be5a5e44b8a22d02889553ec81e
ali <alireza.naghavi0@gmail.com>
2024-09-12 20:07:44 +0330

    changed the returant to tallar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import image from "./assets/react.svg";
-import weddPic from "./assets/wedd.jpg";
+import weddPic from "./assets/wedd-comp.jpg";
 import BackgroundVid from "./assets/unblur-trim.mp4";
 
 const App = () => {
-  // const [anything] = useSound(mySound)
+  
 
   return (
     <div className="w-screen h-screen bg-blue-950 flex items-center justify-center flex-col">
       <div className="min-h-screen relative overflow-hidden">
-        <video className="absolute left-0 top-0 w-full h-full object-cover" autoPlay muted>
+        <video className="absolute left-0 top-0 w-full h-full object-cover" autoPlay muted >
           <source src={BackgroundVid} type="video/mp4" />
         </video>
         <div className="backdrop-blur-md backdrop-brightness-50 w-screen h-screen text-white flex items-center justify-center">
